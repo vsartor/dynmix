b22df6cbbc4dc3458eba32c1bb8185a768789e69
Victhor Sartório <vct.sartor@gmail.com>
2018-11-22 23:19:40 -0200

    Partial progress on backwards estimator for the DP

--- dynmix/dirichlet.py
+++ dynmix/dirichlet.py
@@ -59,3 +59,77 @@ def dirichlet_backwards_sampler(c, delta):
         u = npr.dirichlet((1 - delta) * c[t-1], 1)[0]
         omega[t-1] = S * omega[t] + (1 - S) * u
     return omega
+
+
+def mod_dirichlet_mode(c, a, b):
+    '''
+    Returns the mode of a Mod-Dirichlet distribution from Appendix A.
+
+    Args:
+        c: The Dirichlet parameter.
+        a: The minimum parameter.
+        b: The maximum parameter.
+    
+    Returns:
+        The mode vector.
+    
+    Raises:
+        ValueError: if the mode for S does not exist.
+    '''
+
+    return (b - a) * (c - 1) / (c - 1).sum() + a
+
+
+def mod_dirichlet_parameters(delta, c, omega):
+    '''
+    Returns the parameter set for the Mod-Dirichlet from
+    Proposition 3.2. Internal helper function.
+
+    Args:
+        delta: Discount factor for the model.
+        c: The information parameter for the time of interest.
+        omega: The mode from one time instant ahead.
+
+    Raises:
+        ValueError: if the mode for S does not exist.
+    '''
+
+    k = omega.size
+
+    # Step 1. Calculate the mode for S
+    
+    c_sum = c.sum()
+    alpha = delta * c_sum
+    beta = (1 - delta) * c_sum
+
+    if alpha <= 1 or beta <= 1:
+        raise ValueError('Mode for S does not exist')
+    
+    s = (alpha - 1) / (alpha + beta - 2)
+    
+    # Step 2. Calculate the arguments and the mode for S
+
+    c = (1 - delta) * c
+    a = s * omega
+    b = (1 - s) * np.ones(k) + s * omega
+    
+    return c, a, b
+
+
+def dirichlet_backwards_estimator(c, delta):
+    '''
+    Instead of performing usual backwards sampling, it iterates
+    backwards picking values for omega that maximize its distribution,
+    i.e. it takes the modes instead of generating samples.
+
+    Args:
+        c: The online distribution parameters obtained from forward
+            filtering.
+        delta: The discount factor for the model.
+    
+    Returns:
+        A matrix containing modes from the posterior distribution
+        of the Dirichlet states.
+    '''
+
+    pass
