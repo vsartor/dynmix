c467c42cca39ddf395cff123cd24ec4593bc2c72
Victhor Sartório <vct.sartor@gmail.com>
2018-11-25 20:44:12 -0200

    Put function in the right place

--- dynmix/sdmmm.py
+++ dynmix/sdmmm.py
@@ -41,7 +41,7 @@ def sdmmm_likelihood(y, Z, eta, delta, theta, phi, phi_w):
     return 0
 
 
-def sdmmm_estimator(y, k, delta = 0.9, numit = 100):
+def sdmmm_estimator(y, k, init_level, delta = 0.9, numit = 100):
     '''
     Simple Dynamic Membership Mixture Model. A level-based mixture
     model with a first-order polynomial DLM for each cluster. This
@@ -51,6 +51,7 @@ def sdmmm_estimator(y, k, delta = 0.9, numit = 100):
         y: An array with each row being the time-series from one
             observational unit.
         k: Number of clusters.
+        init_level: The initial level of each cluster.
         delta: The universal discount factor to be used for all the
             units.
         numit: Number of iterations for the algorithm to run.
@@ -62,22 +63,16 @@ def sdmmm_estimator(y, k, delta = 0.9, numit = 100):
     n, T = y.shape
 
     #-- Initialize the parameters
-    #TODO: Allow user initializations
+    #TODO: Allow more user initializations
 
     # DLM parameters
     phi = np.ones(k)
     phi_w = np.ones(k)
-    theta = np.tile(npr.normal(0, 20, k), (T, 1)).T
+    theta = np.tile(init_level, (T, 1)).T
 
     # Dirichlet Process parameters
     eta = np.tile(npr.dirichlet(np.ones(k)), (n, T, 1))
     Z = np.tile(npr.multinomial(1, np.ones(k)/k), (n, T, 1))
-
-    # To make sure at least one observation is in each cluster, manually set
-    # each of the first k units into each one of the k clusters
-    for i in range(k):
-        Z[i] = np.zeros((T, k))
-        Z[i,:,i] = np.ones(T)
 
     # Likelihood
     U = np.empty(numit)
@@ -89,20 +84,20 @@ def sdmmm_estimator(y, k, delta = 0.9, numit = 100):
     #-- Iterative updates of parameter estimates based on means
 
     for l in range(numit):
-        print(f'sdmmm_estimator: iteration {i} out of {numit}')
+        print(f'sdmmm_estimator: iteration {l} out of {numit}')
 
         # Update membership dummy parameters for each unit
         for i in range(n):
             for t in range(T):
-                probs = sps.norm.pdf(y[i,t], theta[:,t], 1. / np.sqrt(phi[k]))
-                params = eta * probs
+                probs = sps.norm.pdf(y[i,t], theta[:,t], 1. / np.sqrt(phi))
+                params = eta[i,t] * probs
                 Z[i,t] = np.zeros(k)
-                Z[i,params.argmax()] = 1
+                Z[i,t,params.argmax()] = 1
 
         # Update Dirichlet states for each unit
         for i in range(n):
-            c = dirichlet_forward_filter(Z, delta, np.ones(k) * 0.1)
-            eta = dirichlet_backwards_estimator(c, delta)
+            c = dirichlet_forward_filter(Z[i], delta, np.ones(k) * 0.1)
+            eta[i] = dirichlet_backwards_estimator(c, delta)
 
         # Update DLM states and parameters for each cluster
         for j in range(k):
@@ -113,17 +108,17 @@ def sdmmm_estimator(y, k, delta = 0.9, numit = 100):
                 YJ.append(y[mask,t])
 
             # Update states
-            V = np.array([[1 / np.sqrt(phi)]])
-            W = np.array([[1 / np.sqrt(phi_w)]])
+            V = np.array([[1 / np.sqrt(phi[j])]])
+            W = np.array([[1 / np.sqrt(phi_w[j])]])
             filters = dlm_multi_filter(YJ, F, G, V, W)
-            theta[j], _ = dlm_smoother(G, *filters)
+            theta[j] = dlm_smoother(G, *filters)[0][:,0]
 
             # Update parameters
             observation_ssq = 0
             for t in range(T):
                 observation_ssq += np.sum((YJ[t] - theta[j,t])**2)
-            phi = n / observation_ssq
-            phi_w = np.mean((theta[k,:-1] - theta[k,1:])**2)
+            phi[j] = n / observation_ssq
+            phi_w[j] = np.mean((theta[j,:-1] - theta[j,1:])**2)
 
         # Update likelihood
         # U[l] = sdmmm_likelihood(y, Z, eta, delta, theta, phi, phi_w)
