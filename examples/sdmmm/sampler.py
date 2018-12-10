4c243764ccba3753b99a2bf04a80cbb324c9beeb
Victhor Sartório <vct.sartor@gmail.com>
2018-12-10 19:10:51 -0200

    Update SDMMM examples

--- examples/sdmmm/sampler.py
+++ examples/sdmmm/sampler.py
@@ -7,15 +7,23 @@ import dynmix as dm
 
 k = 2
 T = 10
-n = 5
+n = 9
 
 sim_eta = np.array(
     [[[1., 0.], [1., 0.], [1., 0.], [1., 0.], [1., 0.],
       [1., 0.], [1., 0.], [1., 0.], [1., 0.], [1., 0.]],
      [[1., 0.], [1., 0.], [1., 0.], [1., 0.], [1., 0.],
       [1., 0.], [1., 0.], [1., 0.], [1., 0.], [1., 0.]],
+     [[1., 0.], [1., 0.], [1., 0.], [1., 0.], [1., 0.],
+      [1., 0.], [1., 0.], [1., 0.], [1., 0.], [1., 0.]],
+     [[1., 0.], [1., 0.], [1., 0.], [1., 0.], [1., 0.],
+      [1., 0.], [1., 0.], [1., 0.], [1., 0.], [1., 0.]],
      [[1., .0], [.9, .1], [.8, .2], [.7, .3], [.6, .4],
       [.5, .5], [.4, .6], [.3, .7], [.2, .8], [.1, .9]],
+     [[0., 1.], [0., 1.], [0., 1.], [0., 1.], [0., 1.],
+      [0., 1.], [0., 1.], [0., 1.], [0., 1.], [0., 1.]],
+     [[0., 1.], [0., 1.], [0., 1.], [0., 1.], [0., 1.],
+      [0., 1.], [0., 1.], [0., 1.], [0., 1.], [0., 1.]],
      [[0., 1.], [0., 1.], [0., 1.], [0., 1.], [0., 1.],
       [0., 1.], [0., 1.], [0., 1.], [0., 1.], [0., 1.]],
      [[0., 1.], [0., 1.], [0., 1.], [0., 1.], [0., 1.],
@@ -38,26 +46,34 @@ for i in range(n):
     for t in range(T):
         y[i,t] = np.dot(sim_eta[i,t], npr.normal(sim_theta[:,t], np.sqrt(sim_var)))
 
-plt.plot(sim_theta.T, 'k')
+plt.figure(figsize = (8, 2.5))
+#plt.plot(sim_theta.T, 'k')
 for i in range(n):
     plt.plot(y[i], 'g--')
 plt.show()
 
-eta, Z, theta, phi, phi_w = dm.sdmmm.sampler(y, 2, 0.7, np.array([5., -5.]))
+eta, Z, theta, phi, phi_w = dm.sdmmm.sampler(y, 2, 0.2, np.array([5., -5.]))
 
-
-plt.plot(sim_theta.T, 'k')
-plt.plot(theta.mean(axis=0).T, 'r')
-plt.plot(np.quantile(theta, 0.90, axis=0).T, 'r', alpha = 0.4)
-plt.plot(np.quantile(theta, 0.10, axis=0).T, 'r', alpha = 0.4)
-plt.plot(np.quantile(theta, 0.99, axis=0).T, 'r', alpha = 0.2)
-plt.plot(np.quantile(theta, 0.01, axis=0).T, 'r', alpha = 0.2)
+plt.figure(figsize = (8, 2.5))
+eta_est = eta.mean(axis=0)
+for i in range(n):
+    if i != 4:
+        plt.plot(y[i], color = (eta_est[i,t,0],0,1-eta_est[i,t,0]))
+for t in range(T):
+    plt.scatter(t, y[4,t], color = (eta_est[4,t,0],0,1-eta_est[4,t,0]))
 plt.show()
 
-for i in range(n):
-    plt.subplot(3, 2, i+1)
-    plt.plot(sim_eta[i], 'k')
-    plt.plot(eta.mean(axis=0)[i], 'r')
-    plt.plot(np.quantile(eta, 0.90, axis=0)[i], 'r', alpha = 0.4)
-    plt.plot(np.quantile(eta, 0.10, axis=0)[i], 'r', alpha = 0.4)
+plt.figure(figsize = (8, 2.5))
+plt.plot(sim_theta.T, 'k')
+plt.plot(theta.mean(axis=0).T, 'r')
+plt.plot(np.quantile(theta, 0.95, axis=0).T, 'r', alpha = 0.4)
+plt.plot(np.quantile(theta, 0.05, axis=0).T, 'r', alpha = 0.4)
+plt.show()
+
+eta_gibbs = eta
+plt.figure(figsize = (8, 2.5))
+plt.plot(sim_eta[4,:,0], 'k')
+plt.plot(np.quantile(eta_gibbs, 0.5, axis=0)[4,:,0], 'r')
+plt.plot(np.quantile(eta_gibbs, 0.95, axis=0)[4,:,0], 'r', alpha=0.4)
+plt.plot(np.quantile(eta_gibbs, 0.05, axis=0)[4,:,0], 'r', alpha=0.4)
 plt.show()
