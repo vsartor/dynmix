cc7d690ebc2788fbc85d8f87a2ea508a6c7a775e
Victhor Sartório <vct.sartor@gmail.com>
2019-04-29 11:08:45 -0300

    Version 0.2.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='dynmix',
-    version='0.1.0',
+    version='0.2.0',
     description='Dynamix Membership Mixture Models',
     long_description=long_description,
     long_description_content_type='text/markdown',
