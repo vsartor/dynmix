14f08faf99df49efd2fd3207db40edf5e9335aeb
Victhor Sartório <vct.sartor@gmail.com>
2019-06-11 20:08:49 -0300

    [dynamic] Add estimator with given deltas

--- dynmix/dynamic.py
+++ dynmix/dynamic.py
@@ -10,76 +10,74 @@ Copyright notice:
 '''
 
 import numpy as np
-import numpy.random as npr
+import numpy.random as rng
 import scipy.stats as sps
 
 from . import dlm
+from . import common
 from . import dirichlet
 
 
-def estimator(y, k, delta, init_level, numit=10):
+def estimator(Y, F_list, G_list, delta, numit=10, mnumit=100, numeps=1e-6, M=200):
     '''
-    Obtain point estimates for a simple univariate first-order polynomial
-    DLM dynamic clustering.
+    Uses Expectation-Maximization to estimate dynamic clusterization of n
+    m-variate time-series, all observed throughout the same T time instants.
+
     Args:
-        y: An array with each row being the time-series from one
-           observational unit.
-        delta: The universal discount factor to be used for all the
-               units.
-        k: Number of clusters.
-        init_level: The initial level of each cluster.
+        Y: A matrix with T rows and n*m columns.
+        F_list: A list with k specifications for the F matrix of each cluster.
+        G_list: A list with k specifications for the G matrix of each cluster.
+        delta: Vector with discount factor for each observation.
         numit: Number of iterations for the algorithm to run.
+        mnumit: Maximum number of iterations for the M-step algorithm to run.
+        numeps: Numerical precision for the M-step algorithm.
+        M: Number of Monte-Carlo simulations of dummy variables.
+
     Returns:
-        The evolution of the estimates for each parameter.
+        eta: A list with the eta for each time-series.
+        theta: A list with the theta for each cluster.
+        phi: A list with the phi for each cluster.
+        W: A list with the W for each cluster.
     '''
 
-    # Constants
-    n, T = y.shape
-    F = G = np.array([[1]])
+    #-- Preamble 
 
-    # Initialize DLM parameters
-    phi = np.ones(k)
-    phi_w = np.ones(k)
-    theta = np.tile(init_level, (T, 1)).T
+    k, _, _, n, T, _ = common.get_dimensions(Y, F_list, G_list)
+    _, theta, phi, eta = common.initialize(Y, F_list, G_list, dynamic=True)
+    
+    c0 = np.ones(k)
+    mc_estimates = np.empty((T, k))
 
-    # Initialize Dirichlet Process parameters
-    eta = np.tile(npr.dirichlet(np.ones(k)), (n, T, 1))
-    Z = np.tile(npr.multinomial(1, np.ones(k)/k), (n, T, 1))
+    #-- Algorithm
 
-    # Perform iterative updates of parameter estimates based on means
-    for _ in range(numit):
-        # Update membership dummy parameters for each unit
+    for it in range(numit):
+        print(f'\nIteration {it} out of {numit}', end = '')
+        
+        # Step 0: Expectation step
+
+        weights = common.compute_weights_dyn(Y, F_list, G_list, theta, phi, eta)
+        
+        # Step 1: Maximize the weights for each observation
+
         for i in range(n):
-            for t in range(T):
-                probs = sps.norm.pdf(y[i, t], theta[:, t], 1. / np.sqrt(phi))
-                params = eta[i, t] * probs
-                Z[i, t] = np.zeros(k)
-                Z[i, t, params.argmax()] = 1
+            print('.', end='')
 
-        # Update Dirichlet states for each unit
-        for i in range(n):
-            c = dirichlet.forward_filter(Z[i], delta, np.ones(k) * 0.1)
-            eta[i] = dirichlet.backwards_estimator(c, delta)
+            # Simulate M observations and obtain the mean of all M estimates
+            mc_estimates[:,:] = 0
+            for _ in range(M):
+                mc_Y = np.array([rng.multinomial(1, x) for x in weights[:,i,:]])
+                c = dirichlet.forward_filter(mc_Y, delta[i], c0)
+                mc_estimates += dirichlet.backwards_estimator(c, delta[i]) / M
+            eta[:,i,:] = mc_estimates
 
-        # Update DLM states and parameters for each cluster
+        # Step 2: Maximize the cluster parameters
+
         for j in range(k):
-            # Create observation list for multi_dlm
-            YJ = [y[Z[:, t, j] == 1, t] for t in range(T)]
+            theta[j], V, _, _ = dlm.dynamic_weighted_mle(Y, F_list[j], G_list[j], eta[:,:,j],
+                                                         maxit=mnumit, numeps=numeps)
+            phi[j,:] = 1 / np.diag(V)
 
-            # Update states
-            V = np.array([[1 / np.sqrt(phi[j])]])
-            W = np.array([[1 / np.sqrt(phi_w[j])]])
-            filters = dlm.multi_filter(YJ, F, G, V, W)
-            theta[j] = dlm.smoother(G, *filters)[0][:, 0]
-
-            # Update parameters
-            observation_ssq = 0
-            for t in range(T):
-                observation_ssq += np.sum((YJ[t] - theta[j, t])**2)
-            phi[j] = n / observation_ssq
-            phi_w[j] = np.mean((theta[j, :-1] - theta[j, 1:])**2)
-
-    return eta, Z, theta, phi, phi_w
+    return eta, theta, phi
 
 
 def sampler(y, k, delta, init_level, numit=4500, burnin=500):
@@ -134,7 +132,7 @@ def sampler(y, k, delta, init_level, numit=4500, burnin=500):
                 # f_vals = np.exp(-(y[i, t] - theta[l - 1, :, t])**2 / (2*sd**2))
                 f_vals = sps.norm.pdf(y[i, t], theta[l-1, :, t], sd)
                 weights = eta[l-1, i, t] * f_vals
-                Z[l, i, t] = npr.multinomial(1, weights / weights.sum())
+                Z[l, i, t] = rng.multinomial(1, weights / weights.sum())
 
         # Sample Dirichlet states for each unit
         for i in range(n):
@@ -151,7 +149,7 @@ def sampler(y, k, delta, init_level, numit=4500, burnin=500):
             W = np.array([[sd_w[j] ** 2]])
             filters = dlm.multi_filter(YJ, F, G, V, W)
             s, S = dlm.smoother(G, *filters)
-            theta[l, j] = npr.normal(s[:, 0], np.sqrt(S[:, 0, 0]))
+            theta[l, j] = rng.normal(s[:, 0], np.sqrt(S[:, 0, 0]))
 
             # Sample observational precision
             num_obs = 0.0001
@@ -159,13 +157,13 @@ def sampler(y, k, delta, init_level, numit=4500, burnin=500):
             for t in range(T):
                 num_obs += 1
                 observation_ssq += np.sum((YJ[t] - theta[l, j, t])**2)
-            phi[l, j] = npr.gamma(num_obs / 2., 2. / observation_ssq)
+            phi[l, j] = rng.gamma(num_obs / 2., 2. / observation_ssq)
 
             # Sample evolutional precision
             num_state = T - 1 + 0.0001
             state_ssq = \
                 np.sum((theta[l, j, :-1] - theta[l, j, 1:])**2) + 0.0001
-            phi_w[l, j] = npr.gamma(num_state / 2., 2. / state_ssq)
+            phi_w[l, j] = rng.gamma(num_state / 2., 2. / state_ssq)
 
     # Return chains dropping burnin phase
     return (eta[burnin:], Z[burnin:], theta[burnin:],
